621d71d387fbbda4248e1b68f2f449f12db49501
Igor Colonna <igorcolonna@gmail.com>
2019-10-02 00:32:24 -0300

    Questão 4 Original

---
+++ Q4.py
@@ -0,0 +1,15 @@
+def nob(num_a,num_b):
+    resposta = 0
+    a1 = 0
+    a2 = 0
+    b1 = 0
+    b2 = 0
+    if (num_a >= 1 and num_a <= 99) and (num_b >= 1 and num_b <= 99):
+        a1 = num_a // 10
+        b1 = num_b // 10
+        a2 = num_a % 10
+        b2 = num_b % 10
+        resposta = a1 + b1 + a2 + b2
+    else:
+        print("Valor Inválido")
+    return resposta
