f0446d27690ef14669f70b1517c3899cea8246bf
Igor Colonna <igorcolonna@gmail.com>
2019-10-02 00:42:04 -0300

    Questão 1 Corrigida

--- Q1.py
+++ Q1.py
@@ -1,5 +1,8 @@
 def segundograu(a,b,c):
     delta = (b**2) - (4*a*c)
+    x1 = ((-b) + (delta**0.5))/(2*a)
+    x2 = ((-b) - (delta**0.5))/(2*a)
+    print("As raízes são: ",x1,x2)
     if delta < 0:
         return 0
     else:
@@ -8,4 +11,5 @@ def segundograu(a,b,c):
 a = int(input("Digite o valor de a: "))
 b = int(input("Digite o valor de b: "))
 c = int(input("Digite o valor de c: "))
-segundograu(a,b,c)
+
+print(segundograu(a,b,c))
